866c9c9b4f85c1440f3e6aca9460b37744813122
Ahmad Karambakhsh <ahmad.karambakhsh@gmail.com>
2020-09-22 13:55:28 +0800

    update

--- DR_1/dr_main.py
+++ DR_1/dr_main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import torch
 import torch.nn.functional as F
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping
 from torch.utils import data
 from torchvision.datasets import MNIST
 import torchvision
@@ -11,6 +12,8 @@ from efficientnet_pytorch import EfficientNet
 from PIL import Image
 from sklearn.model_selection import KFold
 from torch.utils.data.sampler import SubsetRandomSampler
+from sklearn.metrics import cohen_kappa_score, precision_score, recall_score, f1_score
+from DR_1.pytorchtools import EarlyStopping_kappa
 
 BASE_TRAIN_PATH = 'E:\Dataset\DR\DeepDr'
 ''' Training Dataset Directory '''
@@ -74,12 +77,18 @@ training_set = Dataset(os.path.join(BASE_TRAIN_PATH, 'merged_tr_vl', 'merged_tr_
 
 train_generator = data.DataLoader(training_set, **params)
 
+def quadratic_kappa(y_hat, y):
+    return torch.tensor(cohen_kappa_score(torch.argmax(y_hat.cpu(),1), y.cpu(), weights='quadratic'),device='cuda:0')
 
 class DRModel(pl.LightningModule):
 
     def __init__(self):
         super().__init__()
         self.model = EfficientNet.from_pretrained('efficientnet-b0', num_classes=5)
+        self.early_stopping = None
+
+    def set_early_stopping(self, early_stopping):
+        self.early_stopping = early_stopping
 
     def forward(self, x):
         x = self.model(x)
@@ -95,8 +104,7 @@ class DRModel(pl.LightningModule):
         metric = pl.metrics.MSE()
         loss = metric(y_hat, y)
         result = pl.TrainResult(loss)
-        # print(loss)
-        result.log('train_loss', loss,prog_bar=True)
+
         return result
 
     def configure_optimizers(self):
@@ -106,14 +114,16 @@ class DRModel(pl.LightningModule):
         x, y = batch
         y_hat = self(x)
         eye = torch.eye(5).cuda()
-        y = eye[y]
-        loss = F.smooth_l1_loss(y_hat, y)
+        yy = eye[y]
+        loss = F.smooth_l1_loss(y_hat, yy)
         result = pl.EvalResult(loss)
-        result.log('val_loss', loss,prog_bar=True)
+        kappa = quadratic_kappa(y_hat, y)
+        # result.log('kappa', kappa, prog_bar=True)
         return result
 
 
-trainer = pl.Trainer(gpus=1, limit_val_batches=0.1,limit_train_batches=0.1)
+early_stopping = None
+
 model = DRModel()
 
 kfold = KFold(5, True, 1)
@@ -132,4 +142,9 @@ for fold, (train_index, val_index) in enumerate(kfold.split(training_set)):
                                              sampler=valid_sampler,
                                              batch_size=batch_size)
 
+    early_stopping = EarlyStopping_kappa(patience=5, verbose=True)
+    model.set_early_stopping(early_stopping)
+    ''' Early Stopping '''
+
+    trainer = pl.Trainer(gpus=1, limit_val_batches=0.1, limit_train_batches=0.1)
     trainer.fit(model, train_loader, val_loader)
