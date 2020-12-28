aa0ecb0183e76c81967e6d67b6021a5e383b6945
Ahmad Karambakhsh <ahmad.karambakhsh@gmail.com>
2020-12-28 11:33:15 +0800

    update

--- DR_YOLO_RF_ISBI/db_gen.py
+++ DR_YOLO_RF_ISBI/db_gen.py
@@ -33,21 +33,21 @@ main_db = pd.read_csv(in_csv_path, keep_default_na=False)
 file = open('arash_yolo_isbi.csv', 'w', newline='')
 # with file:
 # identifying header
-file.header = ['img_name', 'dr', 'level', 'bl_num', 'bl_size', 'he_num', 'he_size']
+file.header = ['img_name', 'dr', 'level', 'bl_num', 'bl_size', 'he_num', 'he_size', 'laser_num', 'laser_size']
 file.writer = csv.DictWriter(file, fieldnames=file.header)
 file.writer.writeheader()
 
 for i in range(len(main_db['image_path'])):
-    url = main_db['image_path'][i]
-    name = url.split(sep='/')[-1]
-    l_name = name.split(sep='.')[0] + '.txt'
+    name = main_db['image_id'][i]
+    l_name = name + '.txt'
     path = os.path.join(in_label_dir, l_name)
 
     bl_co = 0
     he_co = 0
     bl_size = 0
     he_size = 0
-
+    laser_co=0
+    laser_size = 0
     if os.path.isfile(path):
 
         label_file = open(path, 'r')
@@ -57,17 +57,29 @@ for i in range(len(main_db['image_path'])):
             w = float(sline[3])
             h = float(sline[4])
             bl_co = bl_co + 1 if label == 0 else bl_co
-            he_co = he_co + 1 if label == 3 else he_co
             bl_size = bl_size + (w * h) if label == 0 else bl_size
+            he_co = he_co + 1 if label == 3 else he_co
             he_size = he_size + (w * h) if label == 3 else he_size
+            laser_co = laser_co + 1 if label == 5 else laser_co
+            laser_size = laser_size + (w * h) if label == 5 else laser_size
 
         print(bl_co)
         print(bl_size * 50)
+        quality = int(main_db['Overall quality'][i])
         left = main_db['left_eye_DR_Level'][i]
         right = main_db['right_eye_DR_Level'][i]
+        '''check eye side (left or right)'''
+        level = 0
+        if left in (None, ''):
+            level = str(right)
+        else:
+            level = str(left)
 
-        dr = main_db['dr'][i]
-        level = main_db['level'][i]
-        file.writer.writerow({'img_name': name, 'dr': dr, 'level': level,'bl_num':bl_co,'bl_size':bl_size,'he_num':he_co,'he_size':he_size})
+        dr = 'FALSE' if int(level) <= 1 else 'TRUE'
+
+        if quality == 1:
+            file.writer.writerow(
+                {'img_name': name, 'dr': dr, 'level': level, 'bl_num': bl_co, 'bl_size': bl_size, 'he_num': he_co,
+                 'he_size': he_size, 'laser_num': laser_co, 'laser_size': laser_size})
 
     # print(path)
